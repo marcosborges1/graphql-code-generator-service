d63c102f691860d138954741d6548ba17762d6b7
Marcos Vinicius Borges <marcosviniciusborges@Marcoss-MacBook-Air.local>
2022-02-19 15:19:05 -0300

    Schemas Ok, min code

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -5,21 +5,36 @@ const flatten = require("flat");
 const {
 	jsonToSchema,
 } = require("@walmartlabs/json-to-simple-graphql-schema/lib");
-const data = {
-	posts: [{ id: 1, title: ["Lorem Ipsum"], views: 254 }],
-};
-
-const schema = jsonToSchema({ jsonInput: JSON.stringify(data) });
-// console.log(schema.value);
-// console.log(schema["astNode"]["operationTypes"]["type"]);
 
 const resolvers = {
 	Query: {
 		generateGraphQLCode: async (_, { similarities }) => {
+			const schemas = [];
+			similarities.map((similarity) => {
+				const url = similarity.originAPI.url;
+				const urlTarget = similarity.targetAPI.url;
+				const name =
+					similarity.originAPI.name + url.split("/")[url.split("/").length - 2];
+				console.log(name);
+				schemaOrigin = jsonToSchema({
+					jsonInput: JSON.stringify(similarity.originAPI.parametersOut),
+					baseType: "Result" + name,
+				});
+				const nameTarget =
+					similarity.targetAPI.name +
+					urlTarget.split("/")[urlTarget.split("/").length - 2];
+				schemaTarget = jsonToSchema({
+					jsonInput: JSON.stringify(similarity.targetAPI.parametersOut),
+					baseType: "Result" + nameTarget,
+				});
+				schemas.push(schema);
+				schemas.push(schemaTarget);
+			});
+			//Get Resolvers
 			return {
-				schema: "marcos",
-				queries: "vinicius",
-				resolvers: "borges",
+				schema: schemas,
+				queries: queries,
+				resolvers: resolvers,
 			};
 		},
 	},
